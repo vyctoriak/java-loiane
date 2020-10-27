ed7a1c03b877e1ed8391574787a8b160e4f7c03f
Vyctoria <vyctoriak@gmail.com>
2020-10-27 01:01:53 -0300

    adding exercise 13

--- Lista01/ex12.java
+++ Lista01/ex12.java
@@ -1,4 +1,24 @@
 package Lista01;
 
+/*
+
+Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso
+ideal, usando a seguinte fórmula:
+( 72,7 * altura ) - 58
+
+ */
+
+import java.util.Scanner;
+
 public class ex12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite sua altura: ");
+        double altura = scan.nextDouble();
+        scan.close();
+
+        double pesoIdeal = ( 72.7 * altura ) - 58;
+        System.out.println("Seu peso idal é: " + pesoIdeal);
+    }
 }
