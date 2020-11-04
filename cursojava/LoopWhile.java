7837f1e273cdb06d0dce006d114fd769cacd8b30
Vyctoria <vyctoriak@gmail.com>
2020-11-03 23:30:12 -0300

    adding while exercise

---
+++ cursojava/LoopWhile.java
@@ -0,0 +1,17 @@
+package cursojava;
+
+public class LoopWhile {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int max = 10;
+
+        System.out.println("Contando até: " + max);
+
+        while (i <= max) {
+            System.out.println("Valor de i: " + i);
+            i++;
+        }
+        System.out.println(i);
+    }
+}
